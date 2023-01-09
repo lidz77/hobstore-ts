655941c0bf345d8392ac2b02ad3725e8a8fa2973
TrnCngH <hautrancong@gmail.com>
2023-01-09 10:19:42 +0700

    config runners

--- client/src/http-common.ts
+++ client/src/http-common.ts
@@ -2,7 +2,9 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 const createAxiosInstance = (): AxiosInstance => {
   const config: AxiosRequestConfig = {
-    baseURL: "http://localhost:3084/api",
+    // baseURL: "http://localhost:3084/api",
+    baseURL:
+      "http://ec2-13-250-98-155.ap-southeast-1.compute.amazonaws.com:3084/api",
     // baseURL: "http://ec2-54-152-96-109.compute-1.amazonaws.com/:3084/api",
     headers: {
       "Content-type": "application/json",
