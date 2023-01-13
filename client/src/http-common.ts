5693ec911d8d825e0031e4531d9295450c20459f
TrnCngH <hautrancong@gmail.com>
2023-01-13 21:29:53 +0700

    update delete images

--- client/src/http-common.ts
+++ client/src/http-common.ts
@@ -2,9 +2,9 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 const createAxiosInstance = (): AxiosInstance => {
   const config: AxiosRequestConfig = {
-    baseURL: "http://localhost:3084/api",
-    // baseURL:
-    //   "http://ec2-13-250-98-155.ap-southeast-1.compute.amazonaws.com:3084/api",
+    // baseURL: "http://localhost:3084/api",
+    baseURL:
+      "http://ec2-13-250-98-155.ap-southeast-1.compute.amazonaws.com:3084/api",
     headers: {
       "Content-type": "application/json",
     },
